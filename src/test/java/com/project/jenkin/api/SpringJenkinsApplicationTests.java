09af680e0b2ac8c6660255123de015c70ff493fe
anveerhosalli <anaveerhosalli123@gmail.com>
2025-02-17 23:08:58 +0530

    Added jenkins

---
+++ src/test/java/com/project/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.project.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	@Test
+	public void contextLoads() {
+		logger.info("Execting test case");
+		assertEquals(true, true);
+	}
+
+}
